43309c856d46ceb54a3e8ab685c62fec239b7af3
Developer Baboo <babooheerani@gmail.com>
2023-11-28 19:09:26 +0500

    fixed bug

--- src/Read.jsx
+++ src/Read.jsx
@@ -1,3 +1,5 @@
+/* https://www.youtube.com/watch?v=y5NvOade3sk
+36:41 */
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
@@ -5,27 +7,34 @@ import { Link } from 'react-router-dom';
 
 function Read() {
     const { id } = useParams();
-    const [student, setStudent] = useState([])
+    const [student, setStudent] = useState([]);
+
     useEffect(() => {
-        axios.get('http://localhost:8081/read/'+id)
+        axios.get(`http://localhost:8081/read/${id}`)
             .then(res => {
-                console.log(res)
                 setStudent(res.data);
             })
-        .catch(err => console.log(err))
-    }, [])
-  return (
-      <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
-          <div className='w-50 bg-white rounded p-3'>
+            .catch(err => console.log(err));
+    }, [id]);  // Include id as a dependency in the useEffect dependency array
+
+    return (
+        <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+            <div className='w-50 bg-white rounded p-3'>
                 <h2>Student Detail</h2>
-              <h4> Id:  {student[0].ID}</h4>
-              <h4> Name: {student[0].NAME}</h4>
-              <h4>Email: {student[0].EMAIL}</h4>
+                {student.length > 0 ? (
+                    <>
+                        <h4>{student[0]?.ID}</h4>
+                        <h4>{student[0]?.NAME}</h4>
+                        <h4>{student[0]?.EMAIL}</h4>
+                    </>
+                ) : (
+                    <p>Loading...</p>
+                )}
                 <Link to="/" className="btn btn-primary me-2">Back</Link>
                 <button className="btn btn-info">Edit</button>
             </div>
-    </div>
-  )
+        </div>
+    );
 }
 
-export default Read
+export default Read;
